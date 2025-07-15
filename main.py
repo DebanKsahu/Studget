b8b6a1a33799b2abc41494423bff4204b6aa424b
Deban Kumar Sahu <sahudebankumar@gmail.com>
2025-07-15 22:22:19 +0530

    Added CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from Database import close_db, init_db
@@ -13,6 +14,13 @@ async def lifespan(app: FastAPI):
     await close_db()
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 app.include_router(auth_router)
 app.include_router(profile_router)
